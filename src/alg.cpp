3922b1ae8e4cbf63e170411900332c6168c2d8dd
Филимонов Павел <99137277+painerit@users.noreply.github.com>
2022-03-03 22:41:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,7 +11,7 @@ double pown(double value, uint16_t n) {
   if (n == 1) {
     return value;
   }
-  for (int i = 1; i < n; i++)
+  for (int i = 1; i <= n; i++)
     res = res * value;
   return res;
 }
